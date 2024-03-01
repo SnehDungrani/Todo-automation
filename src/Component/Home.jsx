e7811378f34a19a1b93d28d8f10dc6ebe1fd19c6
sneh <snehdungrani000@gmail.com>
2024-03-01 23:04:55 +0530

    Remove: comment and add update functionality

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,74 +1,54 @@
 import React, { useState } from "react";
 import { Button, Form, Input, Modal, notification } from "antd";
-// import Modal from "./Modal";
 import Task from "./Task";
 
 const { TextArea } = Input;
 
 export const Home = () => {
   const [tasks, setTasks] = useState([]);
-
-  // const [title, setTitle] = useState("");
   const [defaultData, setDefaultDataSet] = useState(null);
 
-  // const [description, setDescription] = useState("");
-  // const [editIndex, setEditIndex] = useState(null);
-
-  // const [show, setShow] = useState(false);
   const [modal2Open, setModal2Open] = useState(false);
-  // const [isEditItem, setIsEditItem] = useState();
 
   const [form] = Form.useForm();
 
   const submitHandler = (e) => {
-    let id = new Date().getTime().toString();
     e.preventDefault();
-
-    // if (title.trim() === "" || description.trim() === "") {
-    //   notification.error({
-    //     message: "Please add some task",
-    //     duration: 1,
-    //   });
-    //   return;
-    // }
 
     form
       .validateFields()
       .then((value) => {
-        console.log(value);
-
         if (value !== null) {
+          const id = new Date().getTime().toString();
           const valueWithId = { id, ...value };
-          setTasks((prev) => [...prev, valueWithId]);
-        }
 
-        notification.success({
-          message: "SUCCESS",
-          description: "Task Added successfully",
-          duration: 1,
-        });
-        setModal2Open((pr) => !pr);
-        form.resetFields();
+          if (defaultData !== null) {
+            const updatedTask = tasks.map((task) =>
+              task.id === defaultData.id ? valueWithId : task
+            );
+            setTasks(updatedTask);
+
+            notification.success({
+              message: "SUCCESS",
+              description: "Task Edit successfully",
+              duration: 1,
+            });
+          } else {
+            setTasks((prev) => [...prev, valueWithId]);
+
+            notification.success({
+              message: "SUCCESS",
+              description: "Task Added successfully",
+              duration: 1,
+            });
+          }
+
+          setModal2Open((pr) => !pr);
+          setDefaultDataSet(null);
+          form.resetFields();
+        }
       })
       .catch((err) => console.log(err));
-    // if (editIndex !== null) {
-    //   const updatedTasks = [...tasks];
-
-    //   updatedTasks[editIndex] = { title, description, id };
-    //   setTasks(updatedTasks);
-    //   setEditIndex(null);
-    // } else {
-    //   setIsEdit("Add");
-
-    //   // const newTask = { title, description, id };
-    //   // setTasks([...tasks, newTask]);
-
-    //   // console.log(newTask);
-    // }
-
-    // setTitle("");
-    // setDescription("");
-    // setId(undefined);
   };
 
   console.log(tasks);
@@ -79,18 +59,11 @@ export const Home = () => {
     const taskToEdit = tasks[index];
     form.setFieldsValue(taskToEdit);
     setDefaultDataSet(taskToEdit);
-
-    // setIsEditItem(taskToEdit.id);
-
-    // setTitle(taskToEdit.title);
-    // setDescription(taskToEdit.description);
-    // setEditIndex(index);
   };
 
   const deleteTask = (id) => {
     const updatedTasks = tasks.filter((_, i) => i !== id);
     setTasks(updatedTasks);
-    // setEditIndex(null);
     notification.error({
       message: "DELETE",
       description: "Task Deleted successfully",
@@ -123,7 +96,7 @@ export const Home = () => {
           title="Add your task"
           centered
           open={modal2Open}
-          okText={defaultData !== null ? "edit" : "add"}
+          okText={defaultData !== null ? "Edit" : "Add"}
           onOk={submitHandler}
           onCancel={() => {
             setModal2Open(false);
@@ -144,12 +117,7 @@ export const Home = () => {
                 },
               ]}
             >
-              <Input
-                placeholder="Enter title"
-                required={true}
-                // value={title}
-                // onChange={(e) => setTitle(e.target.value)}
-              />
+              <Input placeholder="Enter title" required={true} />
             </Form.Item>
             <Form.Item
               label="Description"
@@ -171,8 +139,6 @@ export const Home = () => {
                   height: 120,
                   resize: "none",
                 }}
-                // value={description}
-                // onChange={(e) => setDescription(e.target.value)}
               />
             </Form.Item>
             <br />
