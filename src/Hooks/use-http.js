a419679f086b35620d1b4100dd30567702092c4e
sneh <snehdungrani000@gmail.com>
2024-03-11 08:55:30 +0530

    Remove: unnecessary code

--- src/Hooks/use-http.js
+++ src/Hooks/use-http.js
@@ -1,5 +1,4 @@
 import { useState, useCallback } from "react";
-import axios from "axios";
 import { notification } from "antd";
 import Services from "../util/API/service";
 import { deleteAuthDetails } from "../util/API/authStorage";
@@ -49,7 +48,7 @@ const useHttp = () => {
         if (err?.response?.data?.status === 401) {
           deleteAuthDetails();
           localStorage.removeItem("token");
-          window.location.assign('/');
+          window.location.assign("/");
         }
         if (err?.response?.data?.message === "jwt expired") {
           deleteAuthDetails();
@@ -71,78 +70,12 @@ const useHttp = () => {
       }
       setIsLoading(false);
     },
-    [],
+    []
   );
 
-  const sendBulkRequest = useCallback(
-    async (urls, responseHandler, successMessage, errorHandler) => {
-      setIsLoading(true);
-      try {
-        const response = await axios.all(
-          urls?.map((url) => {
-            switch (url?.url?.type) {
-              case "POST":
-                return Services.post(url?.url?.endpoint, url?.payload);
-
-              case "PUT":
-                return Services.put(url?.url?.endpoint, url?.payload);
-
-              case "DELETE":
-                return Services.delete(url?.url?.endpoint);
-
-              case "PATCH":
-                return Services.patch(url?.url?.endpoint, url?.payload);
-
-              default:
-                return Services.get(url?.endpoint);
-            }
-          }),
-        );
-
-        if (successMessage) {
-          notification.success({ message: successMessage, duration: "3" });
-        }
-        try {
-          if (responseHandler) {
-            responseHandler(response);
-          }
-        } catch (e) {
-          console.log(e);
-        }
-      } catch (err) {
-        if (err?.response?.data?.message === "jwt expired") {
-          deleteAuthDetails();
-          localStorage.removeItem("name");
-          localStorage.removeItem("email");
-          localStorage.removeItem("token");
-          window.location.reload();
-        }
-        console.log(err?.response?.data?.message);
-        if (err?.response?.data?.message) {
-          notification.error({
-            message: err?.response?.data?.message,
-            duration: "3",
-          });
-          if (errorHandler) {
-            errorHandler(err?.response?.data?.message);
-          }
-
-          return;
-        }
-
-        // eslint-disable-next-line consistent-return
-        return notification.error({
-          message: "Something Wrong Please Try again",
-        });
-      }
-      setIsLoading(false);
-    },
-    [],
-  );
   return {
     isLoading,
     sendRequest,
-    sendBulkRequest,
   };
 };
 
