d53a09b0ad34b6bd5399f7569f51e6f7a2311422
sneh <snehdungrani000@gmail.com>
2024-03-16 15:17:41 +0530

    Remove: cookie

--- src/util/API/authStorage.js
+++ src/util/API/authStorage.js
@@ -1,16 +1,12 @@
-import { setCookie, eraseCookie } from "../Cookies";
-
 export const getAuthToken = () => {
   return localStorage.getItem("token");
 };
 
 export const setAuthDetails = (accessToken, userName) => {
-  setCookie("SAID", accessToken, 1);
   localStorage.setItem("token", accessToken);
   localStorage.setItem("name", userName);
 };
 
 export const deleteAuthDetails = () => {
-  eraseCookie("SAID");
   localStorage.removeItem("token");
 };
