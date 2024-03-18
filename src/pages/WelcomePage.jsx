25c48622d9947fa224915f344be036d116133e27
sneh <snehdungrani000@gmail.com>
2024-03-18 12:34:57 +0530

    Add: video

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button, Typography } from "antd";
 import { useNavigate } from "react-router-dom";
+import video1 from "./video/todo.mp4";
 
 const { Title, Paragraph } = Typography;
 
@@ -12,15 +13,38 @@ const WelcomePage = () => {
   };
 
   return (
-    <div style={{ textAlign: "center", padding: "50px" }}>
-      <Title level={2}>Welcome to Smart Todo App</Title>
-      <Paragraph>
-        Organize your tasks efficiently with our smart todo application.
-      </Paragraph>
-      <Button type="primary" size="large" onClick={handleGetStarted}>
-        Get Started
-      </Button>
-    </div>
+    <>
+      <div style={{ textAlign: "center", padding: "50px" }}>
+        <Title level={2}>Welcome to Smart Todo App</Title>
+        <Paragraph>
+          Organize your tasks efficiently with our smart todo application.
+        </Paragraph>
+        <Button type="primary" size="large" onClick={handleGetStarted}>
+          Get Started
+        </Button>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <video
+          width="auto"
+          height="auto"
+          src={video1}
+          loop
+          autoPlay
+          style={{
+            borderRadius: "22px",
+            overflow: "hidden",
+            boxShadow:
+              "0 19px 51px 0 rgba(0,0,0,0.16), 0 14px 19px 0 rgba(0,0,0,0.07)",
+            maxWidth: "640px",
+          }}
+        ></video>
+      </div>
+    </>
   );
 };
 
